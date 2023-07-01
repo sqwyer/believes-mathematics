4c38e0d9ddf9992740919b65aaa3d85cc9038619
Sawyer Bivens <sawyer.codes.stuff@gmail.com>
2023-06-30 19:45:47 -0500

    Make responsive

--- src/App.tsx
+++ src/App.tsx
@@ -31,30 +31,30 @@ function App() {
   
   return <div className="min-h-screen" id="home">
     <Nav />
-    <div className="h-[80vh] bg-[#0ED0D9] px-52 pt-[25vh] relative">
+    <div className="h-screen md:h-[70vh] xl:h-[80vh] bg-[#0ED0D9] px-10 md:px-24 lg:px-40 xl:px-52 pt-36 md:pt-[25vh] relative">
       <div className="absolute top-0 left-0 right-0 bottom-0 bg-[#12B9C0] z-10" style={{
         clipPath: "polygon(0 56%, 100% 16%, 100% 100%, 0 100%)"
       }}></div>
-      <img className="absolute right-52 bottom-0 z-30 h-[65vh] select-none" src={hero}></img>
-      <div className="flex flex-col gap-11 z-20">
-        <h1 className="text-6xl font-extrabold w-[800px] text-white z-20 selection:bg-[#0fa0a5]" style={{lineHeight: "70px"}}>Learn Calculus, Number Theory, and Algebra</h1>
-        <p className="text-lg font-semibold w-[374px] text-white z-20 selection:bg-[#0fa0a5]" style={{lineHeight: "22px"}}>I believe that you are capable of any and everything you put your mind to. Take a moment, breathe, and believe.</p>
+      <div className="flex flex-col gap-8 z-20">
+        <h1 className="w-[500px] mx-auto text-center md:text-left md:mx-0 text-4xl xl:text-6xl font-extrabold md:w-[500px] xl:w-[600px] 2xl:w-[800px] text-white z-20 selection:bg-[#0fa0a5]" >Learn Calculus, Number Theory, and Algebra</h1>
+        <p className="lg:text-lg font-semibold w-[450px] text-center md:text-left mx-auto md:mx-0 md:w-[374px] text-white z-20 selection:bg-[#0fa0a5]" >I believe that you are capable of any and everything you put your mind to. Take a moment, breathe, and believe.</p>
       </div>
+      <img className="absolute bottom-0 left-0 right-0 w-full h-[65vh] md:h-[50vh] md:w-auto md:left-auto md:right-10 z-30 xl:right-52 select-none " src={hero}></img>
     </div>
-    <div className="px-52 py-10">
+    <div className="px-10 md:px-24 lg:px-40 xl:px-52 py-10">
       <div className="flex flex-row gap-4 items-baseline">
         <h1 className="font-semibold text-lg selection:bg-gray-300" id="videos">Recent Videos</h1>
         <a className="text-sm cursor-pointer selection:bg-gray-300" href="https://www.youtube.com/@believesmathematics9321/videos" rel="nofollow" target="_blank">(See more)</a>
       </div>
-      {loading == true && <div className="grid grid-cols-3 gap-24 mt-10">
+      {loading == true && <div className="grid grid-rows-3 md:grid-rows-none md:grid-cols-3 gap-6 sm:gap-10 lg:gap-12 xl:gap-24 mt-10">
         <div className="bg-gray-200 animate-pulse duration-100 rounded-lg overflow-hidden w-full aspect-video select-none"></div>
         <div className="bg-gray-200 animate-pulse duration-100 rounded-lg overflow-hidden w-full aspect-video select-none"></div>
         <div className="bg-gray-200 animate-pulse duration-100 rounded-lg overflow-hidden w-full aspect-video select-none"></div>
       </div>}
-      {error == null && <div className="grid grid-cols-3 gap-24 mt-10">
+      {error == null && <div className="grid grid-rows-3 md:grid-rows-none md:grid-cols-3 gap-6 sm:gap-10 lg:gap-12 xl:gap-24 mt-10">
         {videos != null && videos.items.map((item: any, index: number) => <iframe src={`https://www.youtube.com/embed/${item.id.videoId}?enablejsapi=1&origin=http%3A%2F%2Flocalhost%3A5173&widgetid=7`} key={index} className="rounded-lg overflow-hidden w-full aspect-video shadow-sm select-none" />)}
       </div>}
-      {error != null && <div className="grid grid-cols-3 gap-24 mt-10">
+      {error != null && <div className="grid grid-rows-3 md:grid-rows-none md:grid-cols-3 gap-6 sm:gap-10 lg:gap-12 xl:gap-24 mt-10">
         {rememberedRecents.map((item: any, index: number) => <iframe src={`https://www.youtube.com/embed/${item}?enablejsapi=1&origin=http%3A%2F%2Flocalhost%3A5173&widgetid=7`} key={index} className="rounded-lg overflow-hidden w-full aspect-video shadow-sm select-none" />)}
       </div>}
     </div>
